47cc77411caea84335bb454c0f2dfab0b560d7ce
ctfyghuji8 <js1tamia676@hotmail.com>
2024-05-11 21:04:26 +0700

    test

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const name = "john";
-const upperName = name.toUpperCase();
-console.log(upperName); // JOHN
+function isValidBST(root) {
+  return isValid(root, null, null);
+  function isValid(node, min, max) {
+    if (!node) return true;
+    if ((min !== null && node.val <= min) || (max !== null && node.val >= max))
+      return false;
+    return (
+      isValid(node.left, min, node.val) && isValid(node.right, node.val, max)
+    );
+  }
+}
