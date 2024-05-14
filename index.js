43bdbb8f28a6fa8fa9c246ff72c518ddef28376e
ctfyghuji8 <js1tamia676@hotmail.com>
2024-05-14 19:02:26 +0700

    start

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const factorial = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function getHint(secret, guess) {
+  let bulls = 0;
+  let cows = 0;
+  const map = new Map();
+  for (let i = 0; i < secret.length; i++) {
+    if (secret[i] === guess[i]) {
+      bulls++;
+    } else {
+      map.set(secret[i], (map.get(secret[i]) || 0) + 1);
+    }
   }
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
+  for (let i = 0; i < guess.length; i++) {
+    if (secret[i] !== guess[i] && map.has(guess[i]) && map.get(guess[i]) > 0) {
+      cows++;
+      map.set(guess[i], map.get(guess[i]) - 1);
+    }
   }
-  return result;
-};
+  return `${bulls}A${cows}B`;
+}
