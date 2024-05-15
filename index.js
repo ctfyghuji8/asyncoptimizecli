582e04f56b13313578e23934b9d7491ac107c431
ctfyghuji8 <js1tamia676@hotmail.com>
2024-05-15 08:36:07 +0700

    update aptos project

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-function getHint(secret, guess) {
-  let bulls = 0;
-  let cows = 0;
-  const map = new Map();
-  for (let i = 0; i < secret.length; i++) {
-    if (secret[i] === guess[i]) {
-      bulls++;
-    } else {
-      map.set(secret[i], (map.get(secret[i]) || 0) + 1);
-    }
-  }
-  for (let i = 0; i < guess.length; i++) {
-    if (secret[i] !== guess[i] && map.has(guess[i]) && map.get(guess[i]) > 0) {
-      cows++;
-      map.set(guess[i], map.get(guess[i]) - 1);
-    }
-  }
-  return `${bulls}A${cows}B`;
-}
+const groupBy = (arr, func) =>
+  arr
+    .map(typeof func === "function" ? func : (val) => val[func])
+    .reduce((acc, val, i) => {
+      acc[val] = (acc[val] || []).concat(arr[i]);
+      return acc;
+    }, {});
